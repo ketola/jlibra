d6b706aa1bda18812133a30dae574f7291ea7072
Sauli Ketola <sauli.ketola@nitor.fi>
2019-07-15 19:49:55 +0300

    Import BouncyCastleProvider

--- src/main/java/dev/jlibra/example/ImportAccountMnemonicExample.java
+++ src/main/java/dev/jlibra/example/ImportAccountMnemonicExample.java
@@ -1,15 +1,23 @@
 package dev.jlibra.example;
 
-import dev.jlibra.mnemonic.*;
-
 import java.security.Security;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import dev.jlibra.mnemonic.ChildNumber;
+import dev.jlibra.mnemonic.ExtendedPrivKey;
+import dev.jlibra.mnemonic.LibraKeyFactory;
+import dev.jlibra.mnemonic.Mnemonic;
+import dev.jlibra.mnemonic.Seed;
 
 public class ImportAccountMnemonicExample {
 
     public static void main(String[] args) {
-        Security.addProvider(new org.bouncycastle.jce.provider.BouncyCastleProvider());
+        Security.addProvider(new BouncyCastleProvider());
 
-        Seed seed = new Seed(Mnemonic.fromString("legal winner thank year wave sausage worth useful legal winner thank year wave sausage worth useful legal will"), "LIBRA");
+        Seed seed = new Seed(Mnemonic.fromString(
+                "legal winner thank year wave sausage worth useful legal winner thank year wave sausage worth useful legal will"),
+                "LIBRA");
         LibraKeyFactory libraKeyFactory = new LibraKeyFactory(seed);
         ExtendedPrivKey extendedPrivKey = libraKeyFactory.privateChild(new ChildNumber(0));
 
