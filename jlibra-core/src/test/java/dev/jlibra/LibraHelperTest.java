6f94d2f90251b92df368019a58615bc1e82a9e3a
Sauli Ketola <ketola@users.noreply.github.com>
2019-09-29 07:43:27 +0300

    Delegated key rotation capability field to AccountState (#55)
    * added a field for delegated key rotation capability
    
    * Fix tests & examples

--- jlibra-core/src/test/java/dev/jlibra/LibraHelperTest.java
+++ jlibra-core/src/test/java/dev/jlibra/LibraHelperTest.java
@@ -25,7 +25,7 @@ import types.GetWithProof.GetAccountStateResponse;
 public class LibraHelperTest {
 
     private static final String PRIVATE_KEY_HEX = "3051020101300506032b6570042204202b1115484c64c297179d4ec8aa660f09eeae900a1ba6f16423f82869a101c8e98121002e00f50d1ba024895c72a92cee1310dfafefcc826629c266a4c80b914772f82d";
-    private static final String ACCOUNT_STATE_HEX = "010000002100000001217da6c6b3e19f1825cfb2676daecce3bf3de03cf26647c78df00b371b25cc978d000000200000006674633c78e2e00c69fd6e027aa6d1db2abc2a6c80d78a3e129eaf33dd49ce1c5451210100000000000200000000000000200000000577b70d3d2631ba9babe7d05a135f5cc1cbc3d8cfd0a6fc67ad0b56c4fb4cb201000000000000002000000059f1557694ba7d04f578f9db390dfa894ff9bfb66ed12b6263e72fdccb1d9c220300000000000000";
+    private static final String ACCOUNT_STATE_HEX = "010000002100000001217da6c6b3e19f1825cfb2676daecce3bf3de03cf26647c78df00b371b25cc978e000000200000008f5fbb9486acc5fb90f1a6be43a0013d4a7f7f06e3d5fe995be1e9b272c09b5dc0c62d00000000000000030000000000000020000000e7753442710f04596279f6ea097c48782b0683e8b50071e8f2e34be523d7f2c5000000000000000020000000885ae34cf1f18d0c970b416afc7c5475f132511c5f3bbdce4af52ea09a0fd7030000000000000000";
 
     @BeforeClass
     public static void setUpClass() {
@@ -61,11 +61,12 @@ public class LibraHelperTest {
 
         assertThat(accountStates, is(iterableWithSize(1)));
         assertThat(new String(encode(accountStates.get(0).getAccountAddress())),
-                is("6674633c78e2e00c69fd6e027aa6d1db2abc2a6c80d78a3e129eaf33dd49ce1c"));
-        assertThat(accountStates.get(0).getBalanceInMicroLibras(), is(18960724L));
-        assertThat(accountStates.get(0).getReceivedEvents().getCount(), is(2));
-        assertThat(accountStates.get(0).getSentEvents().getCount(), is(1));
-        assertThat(accountStates.get(0).getSequenceNumber(), is(3));
+                is("8f5fbb9486acc5fb90f1a6be43a0013d4a7f7f06e3d5fe995be1e9b272c09b5d"));
+        assertThat(accountStates.get(0).getBalanceInMicroLibras(), is(3000000L));
+        assertThat(accountStates.get(0).getReceivedEvents().getCount(), is(3));
+        assertThat(accountStates.get(0).getSentEvents().getCount(), is(0));
+        assertThat(accountStates.get(0).getSequenceNumber(), is(0));
         assertThat(accountStates.get(0).getDelegatedWithdrawalCapability(), is(false));
+        assertThat(accountStates.get(0).getDelegatedKeyRotationCapability(), is(false));
     }
 }
