4b5a58b41d6fcd3694badcf574ad59c32c37d274
Sauli Ketola <sauli.ketola@nitor.fi>
2019-10-17 19:12:32 +0300

    fix rounding in examples

--- jlibra-examples/src/main/java/dev/jlibra/example/GetAccountStateExample.java
+++ jlibra-examples/src/main/java/dev/jlibra/example/GetAccountStateExample.java
@@ -13,14 +13,12 @@ import dev.jlibra.admissioncontrol.query.UpdateToLatestLedgerResult;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
-import static java.math.RoundingMode.DOWN;
-
 public class GetAccountStateExample {
 
     private static final Logger logger = LogManager.getLogger(GetAccountStateExample.class);
 
     public static void main(String[] args) {
-        String address = "8f5fbb9486acc5fb90f1a6be43a0013d4a7f7f06e3d5fe995be1e9b272c09b5d";
+        String address = "cf1d4e9a20a59cb3b599d2a3302c35d04a9ca7646ddc7d53af4eacad10656e82";
 
         ManagedChannel channel = ManagedChannelBuilder.forAddress("ac.testnet.libra.org", 8000)
                 .usePlaintext()
@@ -40,7 +38,8 @@ public class GetAccountStateExample {
             logger.info("Received events: {}", accountResource.getReceivedEvents().getCount());
             logger.info("Sent events: {}", accountResource.getSentEvents().getCount());
             logger.info("Balance (microLibras): {}", accountResource.getBalanceInMicroLibras());
-            logger.info("Balance (Libras): {}", new BigDecimal(accountResource.getBalanceInMicroLibras()).divide(BigDecimal.valueOf(1000000), DOWN));
+            logger.info("Balance (Libras): {}", new BigDecimal(accountResource.getBalanceInMicroLibras())
+                    .divide(BigDecimal.valueOf(1000000)));
             logger.info("Sequence number: {}", accountResource.getSequenceNumber());
             logger.info("Delegated withdrawal capability: {}", accountResource.getDelegatedWithdrawalCapability());
             logger.info("Delegated key rotation capability: {}", accountResource.getDelegatedKeyRotationCapability());
