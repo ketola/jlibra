9ae598262a1afed94e6d1c1bfc1681bfcc1348e3
zebei <zebei@qq.com>
2019-12-30 15:08:37 +0800

    Example of GetTransactions : Get the committed transactions by version range.

--- jlibra-core/src/main/java/dev/jlibra/admissioncontrol/query/Query.java
+++ jlibra-core/src/main/java/dev/jlibra/admissioncontrol/query/Query.java
@@ -18,16 +18,27 @@ public abstract class Query {
 
     public abstract Optional<List<GetAccountTransactionBySequenceNumber>> getAccountTransactionBySequenceNumberQueries();
 
+    public abstract Optional<List<GetTransactions>> getTransactions();
+
     public List<RequestItem> toGrpcObject() {
-        return Stream.concat(
-                getAccountStateQueries()
-                        .map(Collection::stream)
-                        .orElse(Stream.empty())
-                        .map(GetAccountState::toGrpcObject),
-                getAccountTransactionBySequenceNumberQueries()
-                        .map(Collection::stream)
-                        .orElse(Stream.empty())
-                        .map(GetAccountTransactionBySequenceNumber::toGrpcObject))
-                .collect(toList());
+        Stream<RequestItem> getAccountStateStream = getAccountStateQueries()
+                .map(Collection::stream)
+                .orElse(Stream.empty())
+                .map(GetAccountState::toGrpcObject);
+        Stream<RequestItem> getAccountTransactionsStream =getAccountTransactionBySequenceNumberQueries()
+                .map(Collection::stream)
+                .orElse(Stream.empty())
+                .map(GetAccountTransactionBySequenceNumber::toGrpcObject);
+        Stream<RequestItem> getTransactions = getTransactions()
+                .map(Collection::stream)
+                .orElse(Stream.empty())
+                .map(GetTransactions::toGrpcObject);
+
+        Stream<RequestItem> totalStream = Stream.concat(
+                getAccountStateStream,
+                getAccountTransactionsStream);
+        totalStream= Stream.concat(totalStream, getTransactions);
+
+        return totalStream.collect(toList());
     }
 }
---
+++ jlibra-core/src/main/java/dev/jlibra/admissioncontrol/query/TransactionListWithProof.java
@@ -0,0 +1,28 @@
+package dev.jlibra.admissioncontrol.query;
+
+import org.immutables.value.Value;
+import types.Events;
+
+import java.util.Collection;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
+@Value.Immutable
+public interface TransactionListWithProof {
+    List<Event> getEvents();
+
+    static TransactionWithProof fromGrpcObject(types.TransactionOuterClass.TransactionListWithProof grpcObject) {
+        List<List<Events.Event>> collect = grpcObject.getEventsForVersions().getEventsForVersionList().stream()
+                .map(Events.EventsList::getEventsList)
+                .collect(toList());
+
+        List<Event> eventResult = collect.stream().map(eventGrpcObjects ->
+             eventGrpcObjects.stream().map(Event::fromGrpcObject).collect(toList())
+        ).flatMap(Collection::stream).collect(toList());
+
+        return ImmutableTransactionWithProof.builder()
+                .addAllEvents(eventResult)
+                .build();
+    }
+}
