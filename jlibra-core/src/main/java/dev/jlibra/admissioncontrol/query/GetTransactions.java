675f7be2313e91fd798ac54e08e6a13fa4b67b78
Sauli Ketola <sauli.ketola@nitor.fi>
2020-01-02 19:10:16 +0200

    read transaction data

--- jlibra-core/src/main/java/dev/jlibra/admissioncontrol/query/GetTransactions.java
+++ jlibra-core/src/main/java/dev/jlibra/admissioncontrol/query/GetTransactions.java
@@ -1,13 +1,17 @@
 package dev.jlibra.admissioncontrol.query;
 
 import org.immutables.value.Value;
+
 import types.GetWithProof;
 import types.GetWithProof.RequestItem;
 
 @Value.Immutable
 public abstract class GetTransactions {
+
     public abstract long getStartVersion();
+
     public abstract long getLimit();
+
     public abstract boolean getFetchEvents();
 
     public RequestItem toGrpcObject() {
--- jlibra-core/src/main/java/dev/jlibra/admissioncontrol/query/TransactionListWithProof.java
+++ jlibra-core/src/main/java/dev/jlibra/admissioncontrol/query/TransactionListWithProof.java
@@ -1,25 +1,36 @@
 package dev.jlibra.admissioncontrol.query;
 
-import org.immutables.value.Value;
-import types.Events;
+import static java.util.stream.Collectors.toList;
 
 import java.util.Collection;
 import java.util.List;
 
-import static java.util.stream.Collectors.toList;
+import org.immutables.value.Value;
+
+import dev.jlibra.admissioncontrol.transaction.Transaction;
+import types.Events;
 
 @Value.Immutable
 public interface TransactionListWithProof {
+
     List<Event> getEvents();
 
-    static TransactionWithProof fromGrpcObject(types.TransactionOuterClass.TransactionListWithProof grpcObject) {
+    List<Transaction> getTransactions();
+
+    static TransactionListWithProof fromGrpcObject(types.TransactionOuterClass.TransactionListWithProof grpcObject) {
+
+        List<Transaction> transactions = grpcObject.getTransactionsList().stream()
+                .map(Transaction::fromGrpcObject)
+                .collect(toList());
+
         List<Event> eventResult = grpcObject.getEventsForVersions().getEventsForVersionList().stream()
                 .map(Events.EventsList::getEventsList)
                 .map(eventGrpcObjects -> eventGrpcObjects.stream().map(Event::fromGrpcObject).collect(toList()))
                 .flatMap(Collection::stream).collect(toList());
 
-        return ImmutableTransactionWithProof.builder()
+        return ImmutableTransactionListWithProof.builder()
                 .addAllEvents(eventResult)
+                .addAllTransactions(transactions)
                 .build();
     }
 }
