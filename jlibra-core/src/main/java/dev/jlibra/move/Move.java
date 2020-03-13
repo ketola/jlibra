3b6800c5119078b4c45d1e5a1ad582cbab2db877
Sauli Ketola <sauli.ketola@nitor.fi>
2020-03-13 10:50:41 +0200

    Update transaction scripts from libra repo

--- jlibra-core/src/main/java/dev/jlibra/move/Move.java
+++ jlibra-core/src/main/java/dev/jlibra/move/Move.java
@@ -1,11 +1,9 @@
 package dev.jlibra.move;
 
-import static java.util.stream.Collectors.joining;
-
-import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.charset.StandardCharsets;
+
+import com.google.common.io.ByteStreams;
 
 import dev.jlibra.serialization.ByteArray;
 import dev.jlibra.serialization.ByteSequence;
@@ -26,19 +24,11 @@ public class Move {
 
     private static ByteSequence readMoveScriptBytes(String fileName) {
         InputStream jsonBinary = Move.class.getResourceAsStream(fileName);
-        String json = readJson(jsonBinary);
-        String[] bytesAsString = json.substring(json.indexOf('[') + 1, json.indexOf(']')).split(",");
-        byte[] bytes = new byte[bytesAsString.length];
-        for (int idx = 0; idx < bytesAsString.length; idx++) {
-            bytes[idx] = (byte) Integer.parseInt(bytesAsString[idx]);
+
+        try {
+            return ByteArray.from(ByteStreams.toByteArray(jsonBinary));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        return ByteArray.from(bytes);
     }
-
-    private static String readJson(InputStream inputStream) {
-        return new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))
-                .lines()
-                .collect(joining());
-    }
-
 }
