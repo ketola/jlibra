4973720cdec3e78021715e8e239d7ed8a16c6a76
Sauli Ketola <sauli.ketola@nitor.com>
2020-10-16 15:33:48 +0300

    Use default script for scripts that don't have specific type

--- jlibra-core/src/main/java/dev/jlibra/client/views/transaction/DefaultScript.java
+++ jlibra-core/src/main/java/dev/jlibra/client/views/transaction/DefaultScript.java
@@ -5,7 +5,7 @@ import org.immutables.value.Value;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 @Value.Immutable
-@JsonDeserialize(as = ImmutableCreateChildVaspAccountScript.class)
-public interface CreateChildVaspAccountScript extends Script {
+@JsonDeserialize(as = ImmutableDefaultScript.class)
+public interface DefaultScript extends Script {
 
 }
--- jlibra-core/src/main/java/dev/jlibra/client/views/transaction/Script.java
+++ jlibra-core/src/main/java/dev/jlibra/client/views/transaction/Script.java
@@ -8,13 +8,9 @@ import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import com.fasterxml.jackson.annotation.JsonTypeInfo.As;
 import com.fasterxml.jackson.annotation.JsonTypeInfo.Id;
 
-@JsonTypeInfo(use = Id.NAME, include = As.PROPERTY, property = "type")
+@JsonTypeInfo(use = Id.NAME, include = As.PROPERTY, property = "type", defaultImpl = DefaultScript.class)
 @JsonSubTypes({
-        @JsonSubTypes.Type(value = ImmutableCreateChildVaspAccountScript.class, name = "create_child_vasp_account"),
-        @JsonSubTypes.Type(value = ImmutablePeerToPeerWithMetadataScript.class, name = "peer_to_peer_with_metadata"),
-        @JsonSubTypes.Type(value = ImmutableRotateAuthenticationKeyScript.class, name = "rotate_authentication_key"),
-        @JsonSubTypes.Type(value = ImmutableRotateDualAttestationInfoScript.class, name = "rotate_dual_attestation_info"),
-        @JsonSubTypes.Type(value = ImmutableUnknownTransactionScript.class, name = "unknown_transaction")
+        @JsonSubTypes.Type(value = ImmutablePeerToPeerWithMetadataScript.class, name = "peer_to_peer_with_metadata")
 })
 public interface Script {
 
